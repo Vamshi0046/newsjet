50d16200840b47ef3e2a62037d4393840e5998d0
vamshi <vamshi0046@gmail.com>
2024-08-04 21:04:20 +0530

    bugs fixed

--- newsapp/views.py
+++ newsapp/views.py
@@ -4,13 +4,16 @@ import requests
 def news(request):
     if request.method == 'POST':
         Day=datetime.date.today()
+        Year = datetime.date.today().year
+        # print('year:',Year)
         Today=Day.strftime("%d")
         m=datetime.datetime.now()
         Month=m.strftime("%m")
         n = request.POST.get('news_type')
-        url = f"https://newsapi.org/v2/everything?q={n}&from=2023-{int(Month)-1}-{Today}&sortBy=publishedAt&apiKey=d16f9163c3474ad79ebf3ec9a90d3811"
+        url = f"https://newsapi.org/v2/everything?q={n}&from={Year}-{int(Month)-1}-{Today}&sortBy=publishedAt&apiKey=d16f9163c3474ad79ebf3ec9a90d3811"
         r = requests.get(url)
         news = r.json()
+        # print("news:-->",news)
         articles = news['articles']
         for article in articles:
             article['image_url'] = article.get('urlToImage', '') 
